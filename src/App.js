d98c8d946cfefbdb7f635c9c7f2a03a4d622281c
unknown <you@example.com>
2021-02-19 13:49:13 -0600

    updated webpage

--- src/App.js
+++ src/App.js
@@ -1,17 +1,35 @@
-import React from 'react';
+import React, { useState, useEffect } from 'react'
 import './App.css';
+import axios from 'axios'
+import Character from './components/Character'
+import {BASE_URL} from './constants/indexstarwars'
 
 const App = () => {
-  // Try to think through what state you'll need for this app before starting. Then build out
-  // the state properties here.
+ 
+  const[character, setCharacter] = useState([])
+ 
+  useEffect(() => {
+    axios.get(`${BASE_URL}`)
+    .then(res => {
+      setCharacter(res.data)
+      console.log(res.data)
+    })
+    .catch(err => {
+      console.log(err)
+    })
 
-  // Fetch characters from the API in an effect hook. Remember, anytime you have a 
-  // side effect in a component, you want to think about which state and/or props it should
-  // sync up with, if any.
+  },[])
 
   return (
     <div className="App">
-      <h1 className="Header">Characters</h1>
+      <h1 className="Header">REACT WARS {'>'} </h1>
+      {
+        character.map( fr =>{
+          return <Character character={fr}/>
+        }
+
+        )
+      }
     </div>
   );
 }
--- src/components/Character.js
+++ src/components/Character.js
@@ -1,1 +1,14 @@
-// Write your Character component here
+// Write your Character component 
+import React from 'react';
+
+
+function Character(props) {
+    const {character}= props
+    return (
+        <div>
+            <h1>{character.name}</h1>
+        </div>
+    );
+}
+
+export default Character;
