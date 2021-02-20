6c2e21dfc30913fb495991f573e3400a3118afb5
unknown <you@example.com>
2021-02-19 18:01:45 -0600

    created list

--- src/components/Character.js
+++ src/components/Character.js
@@ -7,6 +7,13 @@ function Character(props) {
     return (
         <div>
             <h1>{character.name}</h1>
+            <p>gender: {character.gender}</p>
+            <p>Height: {character.height}</p>
+            <p>Mass: {character.mass}</p>
+            <p>BirthYear: {character.birth_year}</p>
+            <p>Eye Color: {character.gender}</p>
+            <p>Hair Color: {character.hair_color}</p>
+            <p>Skin Color: {character.skin_color}</p>
         </div>
     );
 }
